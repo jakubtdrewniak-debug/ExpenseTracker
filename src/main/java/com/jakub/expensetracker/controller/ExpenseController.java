888c21b00872263aff37494a71dcb3010e89a878
Kuba <salt-dev@MacBook-Pro-4.local>
2026-03-19 22:32:47 +0100

    added loggers for each class with business logic

--- src/main/java/com/jakub/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/jakub/expensetracker/controller/ExpenseController.java
@@ -4,6 +4,8 @@ import com.jakub.expensetracker.model.Expense;
 import com.jakub.expensetracker.model.ExpenseCategory;
 import com.jakub.expensetracker.repository.ExpenseRepository;
 import com.jakub.expensetracker.service.ExpenseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -14,6 +16,7 @@ import java.util.Optional;
 @RequestMapping("/api/expenses")
 public class ExpenseController {
 
+    private static final Logger log = LoggerFactory.getLogger(ExpenseController.class);
     private final ExpenseRepository expenseRepository;
     private final ExpenseService expenseService;
 
@@ -24,56 +27,67 @@ public class ExpenseController {
 
     @GetMapping
     public List<Expense> getAllExpenses() {
+        log.info("GET request to list expenses called");
         return expenseRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Optional<Expense> getExpense(@PathVariable Long id) {
+        log.info("GET request to find a single hero by ID called");
         return expenseRepository.findById(id);
     }
 
     @PostMapping
     public Expense createExpense(@RequestBody Expense expense) {
+        log.info("POST method to create an Expense object called");
         return expenseRepository.save(expense);
     }
 
     @PostMapping("/bulk")
     public List<Expense> createMultipleExpenses(@RequestBody List<Expense> expenses) {
+        log.info("POST method to create multiple Expense objects at once called");
         return expenseRepository.saveAll(expenses);
     }
 
     @DeleteMapping("/{id}")
     public void deleteExpense(@PathVariable Long id) {
+        log.info("DELETE method by ID called");
         expenseRepository.deleteById(id);
+        log.info("Expense object deleted");
     }
 
     @PutMapping("/{id}")
     public Expense updateExpense(@PathVariable Long id, @RequestBody Expense updatedExpense) {
+        log.info("PUT method called to update id");
 
         Expense existingExpense = expenseRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Expense not found with id: " + id));
-
+        log.info("Expense exists");
         existingExpense.setDescription(updatedExpense.getDescription());
         existingExpense.setAmount(updatedExpense.getAmount());
         existingExpense.setCategory(updatedExpense.getCategory());
         existingExpense.setDate(updatedExpense.getDate());
+        log.info("New values set for object");
 
         return expenseRepository.save(existingExpense);
     }
 
     @GetMapping("/total/{year}/{month}")
     public BigDecimal getMonthlyTotal(@PathVariable int year, @PathVariable int month) {
+        log.info("GET total for all expenses, service method called");
         return expenseService.calculateMonthlyTotal(month, year);
     }
 
     @GetMapping("/category/{category}")
-    public List<Expense> expensesByCategory(@PathVariable ExpenseCategory category){
+    public List<Expense> expensesByCategory(@PathVariable ExpenseCategory category) {
+        log.info("GET all Expense objects per Category");
         return expenseRepository.findByCategory(category);
 
     }
 
     @GetMapping("/category/{category}/total")
     public BigDecimal categoryTotal(@PathVariable ExpenseCategory category) {
+        log.info("GET method to get total per category, service is called");
         return expenseService.expenseByCategory(category);
     }
 }
--- src/main/java/com/jakub/expensetracker/service/ExpenseService.java
+++ src/main/java/com/jakub/expensetracker/service/ExpenseService.java
@@ -1,9 +1,13 @@
 package com.jakub.expensetracker.service;
 
+import com.jakub.expensetracker.ExpensetrackerApplication;
 import com.jakub.expensetracker.model.Expense;
 import com.jakub.expensetracker.model.ExpenseCategory;
 import com.jakub.expensetracker.repository.ExpenseRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 
 import java.math.BigDecimal;
@@ -14,29 +18,36 @@ public class ExpenseService {
 
     private final ExpenseRepository expenseRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(ExpenseService.class);
+
+
     public ExpenseService(ExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository;
     }
 
     public BigDecimal calculateMonthlyTotal(int month, int year) {
+        log.info("calculateMonthlyTotal method called by the controller.");
         List<Expense> allExpenses = expenseRepository.findAll();
 
         BigDecimal total = BigDecimal.ZERO;
 
-        for(Expense expense : allExpenses) {
+        for (Expense expense : allExpenses) {
             if (expense.getDate().getMonthValue() == month && expense.getDate().getYear() == year) {
                 total = total.add(expense.getAmount());
             }
         }
+        log.info("Total amount calculated.");
         return total;
     }
 
     public BigDecimal expenseByCategory(ExpenseCategory category) {
+        log.info("expenseByCategory method called");
         List<Expense> categoryExpenses = expenseRepository.findByCategory(category);
         BigDecimal total = BigDecimal.ZERO;
-        for(Expense expense : categoryExpenses) {
+        for (Expense expense : categoryExpenses) {
             total = total.add(expense.getAmount());
         }
+        log.info("Total amount by category calculated.");
         return total;
     }
 }
